5524153d6542d2433826cc63b897fdce880e2fa7
duclapjp <duclapjp@gmail.com>
2022-01-13 03:42:36 +0700

    update

--- src/main/java/com/example/demo/repository/IRestaurantRepository.java
+++ src/main/java/com/example/demo/repository/IRestaurantRepository.java
@@ -1,8 +1,11 @@
 package com.example.demo.repository;
-
+import com.example.demo.model.FoodOrder;
 import com.example.demo.model.Restaurant;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
 
 @Repository
 public interface IRestaurantRepository extends JpaRepository<Restaurant, Long> {
